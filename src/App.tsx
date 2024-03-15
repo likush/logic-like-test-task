aa5bf530755bd435c528846586802cb0c8d99037
Lilia Kushnareva <l.kushnareva@locals.org>
2024-03-15 08:57:03 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <a className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </div>
+    );
+}
+
+export default App;
