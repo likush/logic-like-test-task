32f07a53bf01513a86f4cfc5db6b2f6558bec152
Lilia Kushnareva <fjordfield@gmail.com>
2024-03-17 10:44:18 +0100

    refactor code

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -1,14 +1,11 @@
 import React, {ReactElement, useMemo} from 'react';
 import './CourseList.scss';
-import {Course} from '../types/types';
 import CourseCard from './CourseCard';
+import {useSelector} from 'react-redux';
+import {selectedCourses} from '../selectors/selectors';
 
-type Props = {
-    data: Course[]
-}
-
-const CourseList: React.FC<Props> = (props): ReactElement => {
-    const {data} = props;
+const CourseList: React.FC<{}> = (): ReactElement => {
+    const data = useSelector(selectedCourses);
 
     const courses = useMemo(() => {
         return data?.map(item => <CourseCard key={item.id} {...item}/>)
